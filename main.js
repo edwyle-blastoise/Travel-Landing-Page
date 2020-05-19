fc1397091e9e6efd36619d94ad1d06612459d447
mbrandler <marya.brandler@yandex.ru>
2020-05-19 15:43:24 +0300

    Add video and update adaptive layout

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-// Burger handler
+// Header (не работает в Chrome)
+// (function () {
+//     const header = document.querySelector('.header');
+//     window.onscroll = () => {
+//         if (window.pageYOffset > 50) {
+//             header.classList.add('header_active');
+//         } else {
+//             header.classList.remove('header_active');
+//         }
+//     };
+// }());
 
+// Burger handler
 (function () {
     const burgerItem = document.querySelector('.burger-menu');
     const menu = document.querySelector('.nav');
