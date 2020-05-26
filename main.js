3bf7ed1aadfc6e5066d825a469754a2188977432
mbrandler <marya.brandler@yandex.ru>
2020-05-26 18:25:36 +0300

    Add scrolling

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-// Header (не работает в Chrome)
-// (function () {
-//     const header = document.querySelector('.header');
-//     window.onscroll = () => {
-//         if (window.pageYOffset > 50) {
-//             header.classList.add('header_active');
-//         } else {
-//             header.classList.remove('header_active');
-//         }
-//     };
-// }());
-
 // Burger handler
 (function () {
     const burgerItem = document.querySelector('.burger-menu');
     const menu = document.querySelector('.nav');
     const close = document.querySelector('.nav-close');
+    const menuLinks = document.querySelectorAll('.header-link');
 
     burgerItem.addEventListener('click', () => {
         menu.classList.add('active');
@@ -23,6 +12,32 @@
     close.addEventListener('click', () => {
         menu.classList.remove('active')
     });
+    if (window.innerWidth <= 767) {
+        for (link of menuLinks) {
+            link.addEventListener('click', () => {
+                menu.classList.remove('active');
+            })
+        }
+    }
+}());
+
+//Scrolling
+(function () {
+    const anchors = document.querySelectorAll('.js-scroll');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const blockID = anchor.getAttribute('href');
+
+            document.querySelector(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
+    window.onbeforeunload = function() {window.scrollTo(0,0);}
 }());
 
 
